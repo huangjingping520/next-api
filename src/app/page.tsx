cc7c23ed03d5701a4d393f58b4fb027a47ee3134
MerlinAlex <merlinalex@qq.com>
2023-04-01 17:31:42 +0800

    feat: Paragraph and LargeHeading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Paragraph from './components/ui/Paragraph'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='bg-red-500'>hello world</main>
+    <main className='bg-red-500'>
+      <Paragraph size='sm' />
+    </main>
   )
 }
